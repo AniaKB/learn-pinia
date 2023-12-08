d69888cc34159a3af1d8b01785dd67f8d63d8ace
AniaKB <aniakbennett@gmail.com>
2023-12-08 16:11:51 -0500

    adding counter in vue.js

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,35 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <div class="count">
+      {{ count }}
+    </div>
+    <div class="buttons">
+      <button @click="decreaseCount">-</button>
+      <button @click="increaseCount">+</button>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const increaseCount = () => {
+  count.value++
+}
+const decreaseCount = () => {
+  count.value--
+}
+</script>
+
+<style>
+.count {
+  font-size: 60px;
+  margin: 20px;
+}
+.buttons button {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
